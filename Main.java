b8409c90a4191b2341b000d16390a4badbbb98a2
Manato <tacotaco0716@gmail.com>
2018-03-15 15:19:44 +0900

    Speed up

--- Main.java
+++ Main.java
@@ -3,27 +3,10 @@ import java.util.HashMap;
 
 public class Main{
 
-	public HashMap<String, String> WORDS = new HashMap<String, String>();
 	public HashMap<String, String> TABLE = new HashMap<String, String>();
 	public static void main(String[] args){
 		Main main = new Main();
 		try{
-			File file = new File("./word.7b");
-			FileReader filereader = new FileReader(file);
-			BufferedReader br = new BufferedReader(filereader);
-
-			String resultStr = "";
-
-			String str = br.readLine();
-			String[] re;
-			while(str != null){
-				re = str.split("  ");
-
-				main.WORDS.put(re[0], re[1]);
-				
-				str = br.readLine();
-			}
-
 			File file1 = new File("./table.txt");
 			FileReader filereader1 = new FileReader(file1);
 			BufferedReader br1 = new BufferedReader(filereader1);
@@ -42,9 +25,24 @@ public class Main{
 
 			String v = "";
 			for (String s : args) {
-				s = s.replace("!", "").replace("?", "").replace(".", "").replace(",", "");
-				v = main.WORDS.get(s.toUpperCase());
-				if(v != null){
+				s = s.replace("!", "").replace("?", "").replace(".", "").replace(",", "").toUpperCase();
+
+				File file2 = new File("./words/"+String.valueOf(s.charAt(0))+"/word.txt");
+				FileReader filereader2 = new FileReader(file2);
+				BufferedReader br2 = new BufferedReader(filereader2);
+				String str2 = br2.readLine();
+				String[] re2;
+				while (str2 != null) {
+					re2 = str2.split("  ");
+					if(re2[0].equals(s)){
+						v = re2[1];
+						break;
+					}
+					str2 = br2.readLine();
+				}
+
+
+				if(v != ""){
 					String[] w = v.split(" ");
 					String y = "";
 					for (String x : w) {
@@ -52,6 +50,7 @@ public class Main{
 					}
 					System.out.print(y+" ");
 				}
+				v = "";
 			}
 			System.out.println("");
 		}catch(Exception e){
